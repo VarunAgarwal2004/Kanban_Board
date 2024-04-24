d4a3ed5b37e1840e30c126045a7ca4ee3ef9ccd9
VarunAgarwal2004 <varunagarwal4567@gmail.com>
2024-04-24 11:21:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Board from "./components/Board";
+import Column from "./components/Column";
+
+function App() {
+  return (
+    <div className="App">
+        <Board />
+    </div>
+  );
+}
+
+export default App;
